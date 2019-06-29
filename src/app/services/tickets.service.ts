db70a236e6cc0175b022252a4f24942620928a5f
maestrotype <adan3891@gmail.com>
2019-06-29 14:17:54 +0300

    add component ticket

--- src/app/services/tickets.service.ts
+++ src/app/services/tickets.service.ts
@@ -3,15 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {map} from 'rxjs/internal/operators';
 import {ITickets} from '../interfaces/tickets';
-
+import ticketsJSON from './tickets.json';
 
 
 @Injectable()
 
 export class TicketsService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+
+  }
 
   getTickets(): Observable<ITickets[]> {
+    console.log('ticketsJSON', ticketsJSON);
     return this.http.get('http://www.json-generator.com/api/json/get/cqGducpwHS?indent=2').pipe(map(data =>{
       const ticketList = data['tickets'];
       return ticketList.map(function(ticket: any)
